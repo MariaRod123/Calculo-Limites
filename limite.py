cd3532b26c2d6caeacd7823b47af164e0ff2f500
María <72228855+MariaRod123@users.noreply.github.com>
2023-03-10 15:53:24 -0300

    Update limite.py

--- limite.py
+++ limite.py
@@ -1,4 +1,4 @@
 from sympy import limit, oo, Symbol
 t = Symbol('t')  # variable de la función matemática en este caso es t
-y = (t**2 - t + 1)/(t**2 + 1)  # expresión matemática a la cual quiero calcularle el límite
-print('El límite de la función  f(t)= (t^2 - t + 1) / (t^2 + 1)  es :', limit(y, t, oo))
+y = (t**2 - t + 1)/(t**2 + 1)  # expresión matemática a la cual quiero calcularle el límite y=f(t)
+print('El límite de la función  f(t)= (t^2 - t + 1) / (t^2 + 1)  es :', limit(y, t, oo)) # parámetro oo:  indica que estoy calculando el límite de la función para cuando t tiende al +inf
